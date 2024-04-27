be84978b8f9a2e6dac6240e51b3761c72d48b251
Raúl Luján Colilla <mcdrokz@gmail.com>
2024-04-27 14:12:28 +0200

    Api implementation

--- app/src/main/java/es/rlujancreations/habitsapppro/home/presentarion/home/HomeScreen.kt
+++ app/src/main/java/es/rlujancreations/habitsapppro/home/presentarion/home/HomeScreen.kt
@@ -2,7 +2,6 @@
 
 package es.rlujancreations.habitsapppro.home.presentarion.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Settings
@@ -26,7 +24,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
