8a30152aac1ad4cd4f22c7dfc8b6ef77cd976476
Raúl Luján Colilla <mcdrokz@gmail.com>
2024-06-13 16:44:49 +0200

    Modules was created

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidHilt) apply false
     alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.androidLibrary) apply false
 }
---
+++ onboarding/domain/src/main/java/es/rlujancreations/onboarding/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package es.rlujancreations.onboarding.domain
+
+class MyClass {
+}
